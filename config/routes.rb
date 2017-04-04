baba1805f9e90eb392c4339e8d72f61a2f584443
Carson Long <ctlong.970@gmail.com>
2017-04-04 02:10:05 -0700

    feat(remix): basic responsive navbar setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#show', page: 'home'
 
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  'signup' => 'users#new'
+  post 'signup' => 'users#create'
+
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
