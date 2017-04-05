a311d971e26538f2dc1b713912dade041591cf70
Carson Long <ctlong.970@gmail.com>
2017-04-04 21:48:26 -0700

    feat(remix): setup signup page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,44 @@
+class SessionsController < ApplicationController
+  before_action :authenticate, only: [:destroy]
+
+  # GET /login
+  def new
+    return @user = User.new unless logged_in?
+    respond_to do |format|
+      format.html { redirect_to @current_user }
+    end
+  end
+
+  # POST /login
+  def create
+    @user = User.find_by_username(user_params[:username])
+
+    respond_to do |format|
+      if @user
+        if @user.authenticate(user_params[:password])
+          log_in(@user)
+          remember(@user)
+          format.html { redirect_to @user }
+        else
+          @user.errors.add(:password, 'is incorrect')
+        end
+      else
+        @user = User.new
+        @user.errors.add(:email, 'does not exist')
+      end
+      format.html { render :new }
+    end
+  end
+
+  # DELETE /logout
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -28,11 +29,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        log_in(@user)
+        remember(@user)
         format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +44,8 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,19 +55,28 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
+    end
+  end
+
+  def public_view
+    @user = User.find_by_id(params[:id])
+    if @user.present?
+      render 'public_view'
+    else
+      redirect_to root_path
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :phone, :snapchat, :facebook, :linkedin, :instagram, :twitter)
+      params.require(:user).permit(:username, :password, :password_confirmation, :name, :email, :phone, :snapchat, :facebook, :linkedin, :instagram, :twitter)
     end
 end
